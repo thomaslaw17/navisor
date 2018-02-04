c751353259c9ae6e013c1cad154eda2e0c2711f4
Thomas Law <thomas17@connect.hku.hk>
2018-02-04 17:47:09 +0800

    Added firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCAlddh5fK5azhcuo6QHwBFN_RyNu1YoZ8',
+    authDomain: 'navisor-b9b70.firebaseapp.com',
+    databaseURL: 'https://navisor-b9b70.firebaseio.com',
+    projectId: 'navisor-b9b70',
+    storageBucket: 'navisor-b9b70.appspot.com',
+    messagingSenderId: '564965755523'
+  }
 };
