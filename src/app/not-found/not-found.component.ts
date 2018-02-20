cc6dbf7e59aec11764c16ff323db172a2236059d
Thomas Law <thomas17@connect.hku.hk>
2018-02-20 22:48:47 +0800

    Readme file altered with run instructions not-fount conditions added

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./not-found.component.css']
 })
 export class NotFoundComponent implements OnInit {
+  constructor(router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
