0ad553d02b44d2e73ebbd9f3a292b3aea5b62e14
erwin <erwin14@connect.hku.hk>
2018-03-16 23:10:34 +0800

    profileNew

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -103,3 +103,24 @@ import { UtilService } from './util.service';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoUtilsModule } from '../demo-utils/module';
+import { DemoComponent } from './component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule.forRoot(),
+    CalendarModule.forRoot(),
+    DemoUtilsModule
+  ],
+  declarations: [DemoComponent],
+  exports: [DemoComponent]
+})
+export class DemoModule {}
